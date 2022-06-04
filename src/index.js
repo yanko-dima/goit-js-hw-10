52e8051365462901d764794ab66a8ad6c4632ef2
Yanko Dima <yanko.dmitriy@gmail.com>
2022-06-05 01:25:47 +0300

    do search

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import fetchCountries from './js/fetchCountries';
 // import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// console.log(fetchCountries);
-
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
@@ -13,16 +11,12 @@ const refs = {
     countryInfo: document.querySelector('.country-info'),
 };
 
-const {searchBox, countryList, countryInfo} = refs;
+const { searchBox, countryList, countryInfo } = refs;
 
 searchBox.addEventListener('input', onInputSearch);
 
-let name = 'swit';
-
-
 function onInputSearch(e) {
     const inputSearch = e.currentTarget.value.trim();
-    console.dir(inputSearch);
 
     if(inputSearch === '') {
         countryList.innerHTML = '';
@@ -35,46 +29,39 @@ function onInputSearch(e) {
         return fetchCountries(inputSearch)
             .then(data => {
                 onMarckupRender(data);
-                console.log(data);
-                console.log(data.length);
             })
-            .catch(Notify.failure('Oops, there is no country with that name'));
+            .catch(error => Notify.failure('Oops, there is no country with that name'));
     };
 }
 
-// Fetch Data
-// fetch(url)
-//     .then(response => response.json())
-//     .then(console.log)
-
-// fetchCountries(name) {
-// }
-
 function onMarckupRender(countries) {
     if(countries.length > 10) {
+        countryList.innerHTML = '';
         return Notify.info('Too many matches found. Please enter a more specific name');
-        console.log('We found mote ten countries');
     }
 
     if(countries.length > 1) {
-        console.log('We found 2-10 countries');
-
-        const markup = countries
-            .map(({name, flags}) => 
+        const markupList = countries
+            .map(({ name, flags }) => 
             `<li>
-                <img src="${flags.svg}"><span>${name.official}</span>
-            </li>`).join();
+            <img style="padding-right: 8px" src="${flags.svg}" width="40" />${name}
+            </li>`).join('');
 
-        console.log(markup);
+        countryList.innerHTML = markupList;
+        countryInfo.innerHTML = '';
     }
 
     if(countries.length === 1) {
-        console.log('We found One countie');
+        const markupInfo = countries
+            .map(({ name, flags, capital, population, languages }) => 
+            `<ul>
+                <li style="font-size: 32px"><img style="padding-right: 8px" src="${flags.svg}" width="40" /> ${name}</li>
+                <li>Capital: <span>${capital}</span></li>
+                <li>Population: <span>${population}</span></li>
+                <li>Languages: <span>${languages[0].name}</span></li>
+            </ul>`).join('');
+
+        countryList.innerHTML = '';
+        countryInfo.innerHTML = markupInfo;
     }
-
-    if(countries.length === 0) {
-        console.log('Ops, we dont finde nothin, please try again');
-    }
-
-
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,15 @@
 export default function fetchCountries(name) {
 // Feth URL
-const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
 
 // Fetch Data
 return fetch(url)
-    .then(response => response.json());
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+});
 };
 
-    // .then(response => {
-    //     if(!response.ok) {
-    //         throw new Error(response.status);
-    //     }
-
-    //     response.json();
-    // });
